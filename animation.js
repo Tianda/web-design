bd995860a3d89e823b30bba9d1458afaf2ba3cc4
Tianda <th62@zips.uakron.edu>
2018-04-03 18:01:18 +0800

    500px

--- animation.js
+++ animation.js
@@ -3,7 +3,7 @@ function positionMessage() {
     if(!document.getElementById("message")) return false;
     var elem = document.getElementById("message");
     elem.style.position = "absolute";
-    elem.style.left = "50px";
+    elem.style.left = "500px";
     elem.style.top = "100px";
 }
 addLoadEvent(positionMessage);
